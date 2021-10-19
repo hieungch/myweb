bdea04a77532587f82c8aaa2c6ffb071703a0de0
hieungch <hieuntgch18057@fpt.edu.vn>
2021-10-20 06:17:31 +0700

    author add edit update delete

--- routes/authors.js
+++ routes/authors.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Author = require("../models/author");
-
+const Book = require("../models/book");
 // all authors route
 router.get("/", async (req, res) => {
   let searchOptions = {};
@@ -30,30 +30,87 @@ router.post("/", async (req, res) => {
     name: req.body.name,
   });
 
-  // try {
-  //   const newAuthor = await author.save();
-  //   // res.redirect(`authors/${newAuthor.id}`);
-  //   res.redirect(`authors`);
-  // } catch {
-  //   res.render("authors/new", {
-  //     author: author,
-  //     errorMessage: "Error in creating Author!",
-  //   });
-  // }
+  try {
+    const newAuthor = await author.save();
+    res.redirect(`authors/${newAuthor.id}`);
+    //res.redirect(`authors`);
+  } catch {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error in creating Author!",
+    });
+  }
 
-  author.save((err, newAuthor) => {
-    if (err) {
-      //console.log(err);
-      res.render("authors/new", {
-        author: author,
-        errorMessage: "Error in creating Author",
-      });
-    } else {
-      // res.redirect(`authors/${newAuthor.id}`);
-      res.redirect(`authors`);
-    }
-  });
+  // author.save((err, newAuthor) => {
+  //   if (err) {
+  //     //console.log(err);
+  //     res.render("authors/new", {
+  //       author: author,
+  //       errorMessage: "Error in creating Author",
+  //     });
+  //   } else {
+  //     // res.redirect(`authors/${newAuthor.id}`);
+  //     res.redirect(`authors`);
+  //   }
+  // });
   // res.send(req.body.name);
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    const books = await Book.find({ author: author.id }).limit(6).exec();
+    res.render("authors/show", {
+      author: author,
+      booksByAuthor: books,
+    });
+  } catch {
+    //console.log(err);
+    res.redirect("/");
+  }
+});
+
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    res.render("authors/edit", { author: author });
+  } catch {
+    res.redirect("/authors");
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    author.name = req.body.name;
+    await author.save();
+    res.redirect(`/authors/${author.id}`);
+  } catch {
+    if (author == null) {
+      res.redirect("/");
+    } else {
+      res.render("authors/edit", {
+        author: author,
+        errorMessage: "Error in updating Author!",
+      });
+    }
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    await author.remove();
+    res.redirect("/authors");
+  } catch {
+    if (author == null) {
+      res.redirect("/");
+    } else {
+      res.redirect(`/authors/${author.id}`);
+    }
+  }
 });
 
 module.exports = router;
