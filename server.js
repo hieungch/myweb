253a2b0cd20b52e6219827a505086502ada325e1
hieungch <hieuntgch18057@fpt.edu.vn>
2021-11-19 23:50:01 +0700

    register codes

--- server.js
+++ server.js
@@ -7,9 +7,16 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
 const bookRouter = require("./routes/books");
+const User = require("./models/user");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -20,6 +27,60 @@ app.use(methodOverride("_method"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//for login session
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+const initializePassport = require("./passport-config");
+
+initializePassport(
+  passport,
+  (email) => User.findOne(User.email === email),
+  (id) => User.findOne(User.id === id)
+);
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+      role: req.body.role,
+    });
+    await newUser.save();
+    res.redirect("/login");
+    console.log(newUser);
+  } catch {
+    res.redirect("/register");
+  }
+});
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
